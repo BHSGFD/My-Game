8d94e06bc246306325ad2aa24467ae46bb4b3282
Derl <orenerle@gmail.com>
2020-05-09 11:03:02 +0300

    final

---
+++ Assets/_Scripts/LoadFirstScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadFirstScene : MonoBehaviour
+{
+public void loadFirstScene()
+    {
+        
+        SceneManager.LoadScene(0);
+    }
+}
